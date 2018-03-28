8b612c37716901775ef8d1fd030a2f27a7c2661c
victoruugo <victor03016@gmail.com>
2018-03-28 18:24:36 +0200

    Escribimos el Codigo y creamos el Formulario adaptado

--- VHAA1718_tema5_Practica/VHAA1718_tema5_Practica/Form1.Designer.cs
+++ VHAA1718_tema5_Practica/VHAA1718_tema5_Practica/Form1.Designer.cs
@@ -1,47 +1,128 @@
-﻿namespace VHAA1718_tema5_Practica
+﻿namespace WindowsFormsApp1
 {
-    partial class Form1
-    {
-        /// <summary>
-        /// Variable del diseñador necesaria.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Limpiar los recursos que se estén usando.
-        /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Código generado por el Diseñador de Windows Forms
+		#region Código generado por el Diseñador de Windows Forms
 
-        /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1298, 666);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.aluNombre = new System.Windows.Forms.TextBox();
+			this.listaAlumnos = new System.Windows.Forms.TextBox();
+			this.aluNota = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// aluNombre
+			// 
+			this.aluNombre.Location = new System.Drawing.Point(33, 68);
+			this.aluNombre.Name = "aluNombre";
+			this.aluNombre.Size = new System.Drawing.Size(100, 20);
+			this.aluNombre.TabIndex = 0;
+			// 
+			// listaAlumnos
+			// 
+			this.listaAlumnos.Location = new System.Drawing.Point(33, 151);
+			this.listaAlumnos.Multiline = true;
+			this.listaAlumnos.Name = "listaAlumnos";
+			this.listaAlumnos.ReadOnly = true;
+			this.listaAlumnos.ShortcutsEnabled = false;
+			this.listaAlumnos.Size = new System.Drawing.Size(459, 122);
+			this.listaAlumnos.TabIndex = 1;
+			// 
+			// aluNota
+			// 
+			this.aluNota.Location = new System.Drawing.Point(216, 68);
+			this.aluNota.Name = "aluNota";
+			this.aluNota.Size = new System.Drawing.Size(100, 20);
+			this.aluNota.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(360, 65);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(132, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Guardar Alumno";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click_1);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(30, 39);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(82, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Nombre Alumno";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(213, 39);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(30, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Nota";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(30, 125);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(87, 13);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Lista de Alumnos";
+			// 
+			// Form1
+			// 
+			this.ClientSize = new System.Drawing.Size(544, 329);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.aluNota);
+			this.Controls.Add(this.listaAlumnos);
+			this.Controls.Add(this.aluNombre);
+			this.Name = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
-        }
+		}
 
-        #endregion
-    }
+		#endregion
+
+		private System.Windows.Forms.TextBox aluNombre;
+		private System.Windows.Forms.TextBox listaAlumnos;
+		private System.Windows.Forms.TextBox aluNota;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+	}
 }
 
